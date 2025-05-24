0fad2768e94d16ac55fcd3e42317b62559af8e28
github-ci <you@example.com>
2025-05-24 06:48:51 +0000

    update

--- src/JsonRPCCallerBundle.php
+++ src/JsonRPCCallerBundle.php
@@ -3,7 +3,16 @@
 namespace Tourze\JsonRPCCallerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class JsonRPCCallerBundle extends Bundle
+class JsonRPCCallerBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Tourze\DoctrineSnowflakeBundle\DoctrineSnowflakeBundle::class => ['all' => true],
+            \Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle::class => ['all' => true],
+            \Tourze\DoctrineTrackBundle\DoctrineTrackBundle::class => ['all' => true],
+        ];
+    }
 }
